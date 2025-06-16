a9216f546e6a895afe5e5e51b6c5a9a3ce30c5eb
schmingledorp <johndoesstuff03@gmail.com>
2025-06-16 10:33:05 -0400

    getting somewhere

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include <ftxui/screen/screen.hpp>
 
 int main() {
-	return 5;
+	auto screen = ftxui::Screen::Create(
+		ftxui::Dimension::Full(),
+		ftxui::Dimension::Full()
+	);
+
+	auto& pixel = screen.PixelAt(10, 5);
+
+	pixel.character = U'X';
+	pixel.foreground_color = ftxui::Color::Red;
+	pixel.bold = true;
+	screen.Print();
 }
