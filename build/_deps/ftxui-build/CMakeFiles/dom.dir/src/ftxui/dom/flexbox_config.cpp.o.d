d6e3163f2f9375a0d87d515507a121639bc4a8bc
schmingledorp <johndoesstuff03@gmail.com>
2025-06-16 10:39:27 -0400

    removed build

--- build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o.d
+++
@@ -1,4 +0,0 @@
-_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o: \
- /home/schmingledorp/fun/rubiksrays/build/_deps/ftxui-src/src/ftxui/dom/flexbox_config.cpp \
- /usr/include/stdc-predef.h \
- /home/schmingledorp/fun/rubiksrays/build/_deps/ftxui-src/include/ftxui/dom/flexbox_config.hpp
